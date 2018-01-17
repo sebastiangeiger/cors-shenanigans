d81b53bdcd10efebce7f6ad06aca661ba1c6d6cd
Sebastian Geiger <sebastian.geiger@gmail.com>
2018-01-17 13:32:43 -0800

    Proxy stuff

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    console.log("Hello!")
+    fetch("/stitchfix/men")
+			.then(res => res.text())
+      .then(html => console.log("[1] Got response:", html.slice(10000,12000)))
+      .catch(err => console.error("[1] Got error:", err));
+    fetch("https://www.stitchfix.com/men")
+			.then(res => res.text())
+      .then(html => console.log("[2] Got response:", html))
+      .catch(err => console.error("[2] Got error:", err));
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Please open your console</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
